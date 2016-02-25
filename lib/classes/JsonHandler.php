29f06c1e317b5143d146d5f49240c8e48c4da744
Harold Angenent <harold@trendwerk.nl>
2016-02-25 17:42:47 +0100

    Verify path existence

--- lib/classes/JsonHandler.php
+++ lib/classes/JsonHandler.php
@@ -7,22 +7,34 @@ namespace Trendwerk\Acf\ThemeConfig;
 
 final class JsonHandler
 {
+    private $path;
+
     public function __construct()
     {
+        add_action('admin_init', array($this, 'verifyPath'));
         add_filter('acf/settings/load_json', array($this, 'load'));
         add_filter('acf/settings/save_json', array($this, 'save'));
+    }
+
+    public function verifyPath()
+    {
+        $this->path = get_stylesheet_directory() . '/config/acf';
+
+        if (! is_dir($this->path)) {
+            mkdir($this->path, 0775, true);
+        }
     }
 
     public function load($paths)
     {
         unset($paths[0]);
-        $paths[] = get_stylesheet_directory() . '/config/acf';
+        $paths[] = $this->path;
 
         return $paths;
     }
 
     public function save($path)
     {
-        return get_stylesheet_directory() . '/config/acf';
+        return $this->path;
     }
 }
